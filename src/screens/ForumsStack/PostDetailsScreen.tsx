e206f2bfb08d25054b9ed970d545c956dbccfad2
flomamale <flebloch@hotmail.com>
2023-05-20 18:22:37 +0200

    PostDetailScreen erstellt, Navigation/types.d.ts FoumId zu Posts geaddet

--- src/screens/ForumsStack/PostDetailsScreen.tsx
+++ src/screens/ForumsStack/PostDetailsScreen.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
 import AppScreen from '../AppScreen';
 import {PostDetailsScreenProps as Props} from '../../navigation/types';
+import {PROTOTYPE_FORUMS} from '../../features/forums/forums';
+import NormalText from '../../components/atoms/text/NormalText';
+import Container from '../../components/atoms/Container';
+import FullPostItemList from '../../components/organisms/FullPostItemList';
+import FullPostItem from '../../components/molecules/FullPostItem';
 
-const PostDetailsScreen = ({}: Props) => {
-  return <AppScreen />;
+const PostDetailsScreen = ({route}: Props) => {
+  const {
+    params: {postId, forumId},
+  } = route;
+  const forum = PROTOTYPE_FORUMS[forumId];
+  const post = forum.posts[postId];
+  return (
+    <AppScreen scroll={false} padded={false}>
+      {!post ? (
+        <NormalText>Der Post konnte nicht gefunden werden!</NormalText>
+      ) : (
+        <Container>
+          <FullPostItem post={post} />
+          <FullPostItemList posts={post.answers ?? {}} />
+        </Container>
+      )}
+    </AppScreen>
+  );
 };
 
 export default PostDetailsScreen;
