8d83634eeee21c2fc21469c226ea2ec7da984ad0
unknown <samuel.seiser@gmail.com>
2023-05-20 18:06:10 +0200

    Reworked CoursesDetailsScreen

--- src/screens/CoursesStack/CoursesDetailsScreen.tsx
+++ src/screens/CoursesStack/CoursesDetailsScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import AppScreen from '../AppScreen';
 import {CoursesDetailsScreenProps as Props} from '../../navigation/types';
 import NormalText from '../../components/atoms/text/NormalText';
@@ -8,12 +8,18 @@ import SubHeading from '../../components/atoms/text/SubHeading';
 import SubSubHeading from '../../components/atoms/text/SubSubHeading';
 import Container from '../../components/atoms/Container';
 import HorizontalView from '../../components/atoms/HorizontalView';
+import PrimaryButton from '../../components/atoms/buttons/PrimaryButton';
+import navigation from '../../styles/Navigation';
+import ToggleButton from '../../components/molecules/ToggleButton';
+import {View} from 'react-native';
 
 const CoursesDetailsScreen = ({route}: Props) => {
   const {
     params: {courseId},
   } = route;
   const course = PROTOTYPE_COURSES[courseId];
+
+  const [toggleButtonState, setToggleButtonState] = useState(true);
 
   //console.log('AAAAA', courseId);
 
@@ -27,12 +33,31 @@ const CoursesDetailsScreen = ({route}: Props) => {
           <Heading>{course.module}</Heading>
           <SubHeading>{course.title}</SubHeading>
           <NormalText>{course.description}</NormalText>
+          <View style={{paddingTop: 10}}>
+            <Heading>Mentis:</Heading>
+          </View>
+          <HorizontalView>
+            <SubHeading>BLAHAAAAAAA</SubHeading>
+            <SubHeading>BLAHBBBBBBB</SubHeading>
+            <SubHeading>BLAHCCCCCCC</SubHeading>
+            <SubHeading>BLAHDDDDDDD</SubHeading>
+            <SubHeading>BLAHEEEEEEE</SubHeading>
+          </HorizontalView>
 
           <HorizontalView>
-            <SubSubHeading>{course.study}</SubSubHeading>
-            <Heading>{course.module}</Heading>
-            <SubHeading>{course.title}</SubHeading>
-            <NormalText>{course.description}</NormalText>
+            <PrimaryButton
+              onPress={() => {
+                //Zum jeweiligen Forum springen
+                navigation.navigate(course.forum_id);
+              }}>
+              Forum
+            </PrimaryButton>
+            <ToggleButton
+              state={toggleButtonState}
+              setState={setToggleButtonState}
+              stateOnText="Als Menti registrieren"
+              stateOffText="Als Menti entfernen"
+            />
           </HorizontalView>
         </Container>
       )}
