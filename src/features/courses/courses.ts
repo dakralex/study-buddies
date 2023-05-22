2dba0dafbbbfb15aa0539a99c7200f142d6529ef
unknown <samuel.seiser@gmail.com>
2023-05-22 16:52:37 +0200

    Added Courses to the forums

--- src/features/courses/courses.ts
+++ src/features/courses/courses.ts
@@ -40,7 +40,7 @@ export const PROTOTYPE_COURSES: Courses = {
       'd52cc86e-9f83-4f7b-a275-7c09949e67d1',
       '4b233ecc-c056-484f-844d-9d787bb0c335',
     ],
-    description: '',
+    description: 'Übung zur VO Betriebsysteme. Viel Aufwand, wenig Freude.',
     forum_id: '753fb11a-398f-45b3-bb88-450dde4b93ad',
   },
   '051031': {
@@ -69,5 +69,70 @@ export const PROTOTYPE_COURSES: Courses = {
     description:
       'In dieser Lehrveranstaltung werden grundlegende Kenntnisse für Projektmanagement vorgezeigt, die man in einem erfundenen Projekt dann umsetzen soll. Für berufserfahrene Student*innen ist dieses Fach sehr leicht zu absolvieren, da sie damit wahrscheinlich schon in der Realtät zu tun hatten.',
     forum_id: '0305c3b4-654b-493e-ae5e-d27d8e3f96bc',
+  },
+  '051032': {
+    id: '051032',
+    title: 'VU Grundlagen der Intelligenten Systeme',
+    module: 'IDS Intelligente & Datenbanksysteme',
+    study: 'BA Informatik',
+    mentee_ids: [
+      'ffcdcbe8-eaa8-4021-8144-3ca7f90c2cf4',
+      'ed7b9726-3f4c-4e5b-9784-a25526424f52',
+    ],
+    description:
+      'Einführung in die Grundlagen der intelligenten Systeme:\n' +
+      '- Grundbegriffe und die historische Entwicklung der künstlichen Intelligenz,\n' +
+      '- Aussagen- und Prädikatenlogik,\n' +
+      '- Prolog,\n' +
+      '- Suchverfahren,\n' +
+      '- Grundlagen künstlicher neuronaler Netze',
+    forum_id: '7d22e206-5a30-40a4-a41e-450e19f63c3c',
+  },
+  '051030': {
+    id: '051030',
+    title: 'VU Programmiersprachen und -konzepte',
+    module: 'PLC Programmiersprachen und -konzepte',
+    study: 'BA Informatik',
+    mentee_ids: [
+      'd52cc86e-9f83-4f7b-a275-7c09949e67d1',
+      '4751135c-c1e7-41be-ad4b-bc5d9252efe7',
+      '4b233ecc-c056-484f-844d-9d787bb0c335',
+      'dafc64a5-dbf9-48da-95bb-f006899eae37',
+      'ffcdcbe8-eaa8-4021-8144-3ca7f90c2cf4',
+      '673e13b3-2357-4942-af22-aa86b0cf3a24',
+      'ed7b9726-3f4c-4e5b-9784-a25526424f52',
+    ],
+    description:
+      'Die Studenten verstehen und können die Hauptkonzepte verschiedener Programmierparadigmen und -sprachen bewerten, einschließlich objektorientierter Programmierung, funktionaler Programmierung, logischer Programmierung und aspektorientierter Programmierung. Sie können fundierte Entscheidungen über den Einsatz und die Anwendbarkeit verschiedener Sprachen und Konzepte für die Anwendungsentwicklung treffen. Die Studenten erlernen die wichtigsten Methoden und Werkzeuge für den Compiler-Aufbau, die statische Programmanalyse und -optimierung sowie die Laufzeitunterstützung. Die Studenten wenden diese Konzepte und Methoden in praktischen Programmier- und Entwicklungsprojekten an.',
+    forum_id: 'e6b2ee39-c097-4660-bb6b-81a11624562a',
+  },
+  '051132': {
+    id: '051132',
+    title: 'VO Einführung in Numerical Computing',
+    module: 'NUM Einführung in Numerical Computing',
+    study: 'BA Informatik',
+    mentee_ids: [
+      'ffcdcbe8-eaa8-4021-8144-3ca7f90c2cf4',
+      '673e13b3-2357-4942-af22-aa86b0cf3a24',
+      'ed7b9726-3f4c-4e5b-9784-a25526424f52',
+    ],
+    description:
+      'Vermittlung grundlegender Techniken der Numerischen Mathematik (Numerische Lineare Algebra, Interpolation, Numerisches Differenzieren, Quadraturen, Approximation, ...) und deren Anwendung.',
+    forum_id: '39f77b6d-2c4c-4f27-b462-d8d0d4a7cc17',
+  },
+  '051130': {
+    id: '051130',
+    title: 'VO Einführende Statistik',
+    module: 'EST Einführende Statistik',
+    study: 'BA Informatik',
+    mentee_ids: [
+      'd52cc86e-9f83-4f7b-a275-7c09949e67d1',
+      'ffcdcbe8-eaa8-4021-8144-3ca7f90c2cf4',
+      '673e13b3-2357-4942-af22-aa86b0cf3a24',
+      '4b233ecc-c056-484f-844d-9d787bb0c335',
+    ],
+    description:
+      'Die Studenten verfügen über Fähigkeiten empirische Sachverhalte mittels statistischer Basistechniken zu beschreiben und graphisch korrekt zu repräsentieren; sowie über ein prinzipielles Verständnis für die grundlegenden Konzepte der Wahrscheinlichkeitstheorie und der inferenzstatistischen Modellierung und Methodik.',
+    forum_id: '6ab7dc6a-c274-48a9-be2a-23eb45de7051',
   },
 };
